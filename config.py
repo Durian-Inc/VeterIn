09a01793db0b50b735fd9321ab926f1338d78ca4
Clay McGinnis <mcginnis@claymav.com>
2018-02-10 01:43:51 -0600

    added static dir, veteran page and a base template.

--- config.py
+++ config.py
@@ -2,3 +2,13 @@
 
 # Enable Flask's debugging features. Should be False in production
 DEBUG = True
+
+# Define the application directory
+import os
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+
+# Application threads. A common general assumption is
+# using 2 per available processor cores - to handle
+# incoming requests using one and performing background
+# operations using the other.
+THREADS_PER_PAGE = 2
