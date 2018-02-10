a50fb3cad5b2d59d9b1987716fd0fd30d16dbadd
Innocent Niyibizi <25cent9@gmail.com>
2018-02-10 11:34:16 -0600

    Populated Datanase: Merged with develop branch to get the latest views and then populated the database.

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 # views.py
 
-import sqlite3
+import sqlite3 as sql
 from flask import render_template, flash, g
 
 from app import app
@@ -9,19 +9,21 @@ from app.utils import uses_template
 
 DATABASE = 'app/database/vets.db'
 
-def get_db():
-    db = sqlite3.connect(DATABASE)
-    return db
+def get_veteran(username):
+    with sql.connect(DATABASE) as con:
+        cur = con.cursor()
+        cur.execute("IN")
 
+    
 @app.route('/')
 def index():
-    cur = get_db().cursor()
     return render_template("index.html")
 
 
 @app.route('/veteran/<username>', methods=['GET'])
 @uses_template('veteran.html')
 def vetpro(username):
+    veteran = get_veteran(username)
     vet = {
         'username': "daddyd",
         'name': "Derek Hanger",
