5574898f388394ffd25daa24495b473de858e7dd
Innocent Niyibizi <25cent9@gmail.com>
2018-02-10 12:53:16 -0600

    Integrated the simple veteran profile page: A usename will be paseed to a function that returns information about the veteran.

--- app/views.py
+++ app/views.py
@@ -10,12 +10,14 @@ from app.utils import uses_template
 DATABASE = 'app/database/vets.db'
 
 def get_veteran(uname):
-    command = "SELECT * FROM veterans where username= '%s' " %uname
-    print command
-    # with sql.connect(DATABASE) as con:
-    #     cur = con.cursor()
-    #     cur.execute("SELECT * FROM veterans where username= '%s' " %uname)
-
+    vet = None
+    command = "SELECT * FROM veterans where username = '%s' " %uname
+    with sql.connect(DATABASE) as con:
+        cur = con.cursor()
+        cur.execute(command)
+        vet = cur.fetchone()
+        cur.close()
+    return vet
     
 @app.route('/')
 def index():
@@ -25,18 +27,22 @@ def index():
 @app.route('/veteran/<username>', methods=['GET'])
 @uses_template('veteran.html')
 def vetpro(username):
-    veteran = get_veteran(username)
-    vet = {
-        'username': "daddyd",
-        'name': "Derek Hanger",
-        'skills': "code,bees,music stuff,orange chicken",
-        'years_served': 2,
-        'rank': "man",
-        'branch': "emo bitch",
-        'bio': "once from fair town, always from fair town",
-        'image': "derek.png"
+    vet = get_veteran(username)
+    for val in vet:
+        print val
+    print len(vet)
+    veteran = {
+        'username': vet[0],
+        'name': vet[1],
+        'skills': vet[2],
+        'years_served': vet[3],
+        'rank': vet[4],
+        'branch': vet[5],
+        'bio': vet[6],
+        'contact': vet[8],
+        'image': vet[7]
     }
 
     return {
-        'veteran': vet
+        'veteran': veteran
     }
