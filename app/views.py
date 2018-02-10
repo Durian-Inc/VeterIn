f16fbbf19b2a09de2a03fa54eb50faedbba84635
Innocent Niyibizi <25cent9@gmail.com>
2018-02-10 10:44:13 -0600

    Database connection: Using the sqlite3 package to connect to database

--- app/views.py
+++ app/views.py
@@ -1,9 +1,17 @@
 # views.py
 
-from flask import render_template
+import sqlite3
+from flask import render_template, flash, g
 
 from app import app
 
+DATABASE = 'app/database/vets.db'
+
+def get_db():
+    db = sqlite3.connect(DATABASE)
+    return db
+
 @app.route('/')
 def index():
+    cur = get_db().cursor()
     return render_template("index.html")
