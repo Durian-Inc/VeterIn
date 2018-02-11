9861e99e8c56a8ca46e8ff51fc086d1d455156a1
Griffin Melson <griffinm962@gmail.com>
2018-02-11 02:10:22 -0600

    Finished map functionality for real this time

--- app/static/js/map.js
+++ app/static/js/map.js
@@ -1,7 +1,7 @@
 print = console.log;
 
 // Map object
-var map = L.map('map').setView([0, 0], 13);
+var map = L.map('map').setView([37.9485, -91.7715], 13);
 
 // Set map source and attribution
 L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
@@ -12,12 +12,11 @@ var xhttp = new XMLHttpRequest();
 
 const address = window.location.protocol + "//" + window.location.hostname + ":" + window.location.port;
 
-print(address + "/api/waypoints");
+navigator.geolocation.getCurrentPosition(function (position) {
+  map.flyTo([position.coords.latitude, position.coords.longitude], 13);
+});
 
 xhttp.onreadystatechange = function() {
-  print(this.readyState);
-  print(this.status);
-  print(this.responseText);
 	if(this.readyState == 4 && this.status == 200) {
     waypoints = JSON.parse(this.responseText);
     for (var waypoint of waypoints) {
@@ -29,8 +28,6 @@ xhttp.onreadystatechange = function() {
         waypoint.name + "</a>"
       );
     }
-	} else {
-		print("rip you done got wackoed");
 	}
 };
 xhttp.open("GET", address + "/api/waypoints", true);
