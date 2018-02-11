16bab0ead1c587e76681fb32212d56decc458c83
Griffin Melson <griffinm962@gmail.com>
2018-02-10 19:28:25 -0600

    Workin on the map

--- app/static/js/map.js
+++ app/static/js/map.js
@@ -1,6 +1,14 @@
+print = console.log;
+// Map object
 var map = L.map('map').setView([51.505, -0.09], 13);
 
+// Set map source and attribution
 L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
 }).addTo(map);
-L.Control.geocoder().addTo(map);
+// Add geocoder
+geocoder = L.Control.geocoder().addTo(map);
+
+print(L.Control.Geocoder());
+
+geocoder.geocoder.geocode("help")
