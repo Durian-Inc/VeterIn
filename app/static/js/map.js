a16ef4b76dc67dc6cc8e65b8fa54cb878861b6b1
Griffin Melson <griffinm962@gmail.com>
2018-02-10 22:37:08 -0600

    Workin on map

--- app/static/js/map.js
+++ app/static/js/map.js
@@ -1,14 +1,21 @@
 print = console.log;
+
 // Map object
 var map = L.map('map').setView([51.505, -0.09], 13);
+
+var xhttp = new XMLHttpRequest();
+
+xhttp.open("GET", window.location.hostname + "/api/waypoints", true);
+xhttp.onreadystatechange = function() {
+	if(this.readyState == 4 && this.status == 200) {
+		waypoints = JSON.parse(this.responseText);
+	}
+	else {
+		print("rip you done got wackoed");
+	}
+};
 
 // Set map source and attribution
 L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
 }).addTo(map);
-// Add geocoder
-geocoder = L.Control.geocoder().addTo(map);
-
-print(L.Control.Geocoder());
-
-geocoder.geocoder.geocode("help")
